987a28a90bfb8bca5ffe9c5f039142eecb1b7207
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-02 14:55:22 -0600

    Add Iteration1

--- src/main.js
+++ src/main.js
@@ -1,51 +1,75 @@
-// Create variables targetting the relevant DOM elements here 👇
-//Random Title Generator
-var randomTitle = getRandomTitle(titles);
+//iteration1
+
 var mainTitle = document.querySelector(".cover-title");
-
-function getRandomTitle(titles) {
-	return titles[Math.floor(Math.random()*titles.length)];
-}
-
-mainTitle.innerText = randomTitle;
-
-//Random Descriptor Generator
-function getRandomDescriptor(descriptors) {
-	return descriptors[Math.floor(Math.random()*descriptors.length)];
-}
-var randomDescriptor = getRandomDescriptor(descriptors);
-var mainDescriptor = document.querySelector(".tagline-1");
-mainDescriptor.innerText = randomDescriptor
-
-var randomDescriptor = getRandomDescriptor(descriptors);
-var mainDescriptor = document.querySelector(".tagline-2");
-mainDescriptor.innerText = randomDescriptor
-
-//Random Cover Generator
-function getRandomCover(covers) {
-  return covers[Math.floor(Math.random()*covers.length)];
-};
-
-var randomCover = getRandomCover(covers);
 var coverImage = document.querySelector(".cover-image");
-coverImage.src = randomCover;
-
-
-
+var descriptor1 = document.querySelector(".tagline-1");
+var descriptor2 = document.querySelector(".tagline-2");
+var newRandomCoverButton = document.querySelector(".random-cover-button");
 
 // We've provided a few variables below
+
+////// DO NOT TOUCH THE 4 LINES BELOW THIS!
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
+////// DO NOT TOUCH THE 4 LINES ABOVE THIS!
+
 
 // Add your event listeners here 👇
+// WHICH FUNCTIONS GO WHEN YOU DO SOMETHING
 
+//iteration 1
+
+window.addEventListener("load", getRandomCover);
+window.addEventListener("load", getRandomTitle);
+window.addEventListener("load", getRandomDescriptor1);
+window.addEventListener("load", getRandomDescriptor2);
+newRandomCoverButton.addEventListener("click", getRandomCover);
+newRandomCoverButton.addEventListener("click", getRandomTitle);
+newRandomCoverButton.addEventListener("click", getRandomDescriptor1);
+newRandomCoverButton.addEventListener("click", getRandomDescriptor2);
 
 // Create your event handlers and other functions here 👇
+// WHAT EXACTLY HAPPENS WHEN DO SOMETHING - THESE ARE ALL FUNCTIONS
 
+//iteration 1
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random()*array.length);
+};
+
+//iteration 1
+
+////cover image
+
+function getRandomCover() {
+	var randomCoverImageIndex = getRandomIndex(covers);
+  coverImage.src = covers[randomCoverImageIndex];
+}
+
+////title
+
+function getRandomTitle() {
+	var randomTitleImageIndex = getRandomIndex(titles);
+	mainTitle.innerText = titles[randomTitleImageIndex];
+}
+
+////descriptor1
+
+function getRandomDescriptor1() {
+	var randomDescriptorImageIndex1 = getRandomIndex(descriptors);
+	descriptor1.innerText = descriptors[randomDescriptorImageIndex1];
+}
+
+////descriptor2
+
+function getRandomDescriptor2() {
+	var randomDescriptorImageIndex2 = getRandomIndex(descriptors);
+	descriptor2.innerText = descriptors[randomCoverImageIndex2];
+}
 
 // We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+// function getRandomIndex(array) {
+//   return Math.floor(Math.random() * array.length);
+// }
